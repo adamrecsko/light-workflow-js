d9e6cf9cd5b891c1a291fc0f01e45967dec3c2b3
adamrecsko <adam.recsko@gmail.com>
2016-11-17 22:38:58 +0100

    Add RemoteActivityAdapter

--- src/application/activity/adapters/remote-activity-adapter.test.ts
+++ src/application/activity/adapters/remote-activity-adapter.test.ts
@@ -1,6 +1,113 @@
+import {RemoteActivityAdapter} from "./remote-activity-adapter";
+import {ActivityDefinition} from "../activity-definition";
+import {ContextResolutionStrategy} from "../../context/resolution-strategies/resolution-stategy";
+import {DecisionRunContext} from "../../context/decision-run-context";
+import {MockDecisionRunContext} from "../../../testing/mocks/decision-run-context";
+import {ObservableFactory} from "../../observable-factory";
+import {Observable, TestScheduler} from "rxjs";
+import {ChaiTestScheduler} from "../../../testing/helpers/chai-test-scheduler";
+import {Serializer} from "../serializer";
+import {expect} from "chai";
+import * as sinon from "sinon";
+import {ScheduleActivityTaskDecisionAttributes, ActivityType} from "../../../aws/aws.types";
+import {RemoteObservableFactory, RemoteActivityObservable} from "../observable/remote-activity-observable";
+
+
+class MockContextResolutionStrategy implements ContextResolutionStrategy<DecisionRunContext> {
+    getContext(): DecisionRunContext {
+        return null;
+    }
+}
+
+class MockObservableFactory implements RemoteObservableFactory {
+    create(decisionContext: DecisionRunContext, scheduleParameters: ScheduleActivityTaskDecisionAttributes): RemoteActivityObservable {
+        return null;
+    }
+}
+
+class MockSerializer implements Serializer {
+    parse(str: string): any {
+        return null;
+    }
+
+    stringify(obj: any): string {
+        return null;
+    }
+}
+
+
+const testActivityName = 'TestActivityName';
+const testActivityVersion = 'test-version-123';
+const testTaskList = 'test-task-list';
+
 describe('RemoteActivityAdapter', ()=> {
-    it('should create activity observable', ()=> {
+
+    let obsFactory: ObservableFactory<string>;
+    let testScheduler: TestScheduler;
+    let runContext: DecisionRunContext;
+    let testValue: string;
+    let contextResolution: ContextResolutionStrategy<DecisionRunContext>;
+    let mockObservableFactory: ObservableFactory<string>;
+    let definition: ActivityDefinition;
+    let mockSerializer: Serializer;
+
+    beforeEach(()=> {
+        obsFactory = new MockObservableFactory();
+        testScheduler = new ChaiTestScheduler();
+        runContext = new MockDecisionRunContext();
+        contextResolution = new MockContextResolutionStrategy();
+        mockObservableFactory = new MockObservableFactory();
+        definition = new ActivityDefinition(testActivityName);
+        mockSerializer = new MockSerializer();
+        testValue = 'test';
 
 
+        definition.defaultTaskHeartbeatTimeout = '1111';
+        definition.defaultTaskPriority = ' def task priority';
+        definition.defaultTaskScheduleToCloseTimeout = '2222';
+        definition.defaultTaskScheduleToStartTimeout = '3333';
+        definition.defaultTaskStartToCloseTimeout = '4444';
+        definition.description = 'description';
+        definition.heartbeatTimeout = '5555';
+        definition.scheduleToCloseTimeout = '6666';
+        definition.scheduleToStartTimeout = '7777';
+        definition.startToCloseTimeout = '8888';
+        definition.taskPriority = 'task priority';
+        definition.version = testActivityVersion;
+    });
+
+    it('should create activity observable with schedule parameters', ()=> {
+        const nextId = '12234';
+        const testInput = 'test-input';
+        const activityObservable = {};
+        const createStub = sinon.stub().returns(activityObservable);
+        const stringifyStub = sinon.stub().returns(testInput);
+        const getContextStub = sinon.stub().returns(runContext);
+        const nextIdStub = sinon.stub().returns(nextId);
+        const testCallAttributes = [100, 'hello'];
+        const expectedScheduleParameters: ScheduleActivityTaskDecisionAttributes = {
+            activityType: {
+                name: testActivityName,
+                version: testActivityVersion
+            },
+            activityId: nextId,
+            input: testInput,
+            scheduleToCloseTimeout: definition.scheduleToCloseTimeout,
+            taskList: {
+                name: testTaskList
+            },
+            scheduleToStartTimeout: definition.scheduleToStartTimeout,
+            startToCloseTimeout: definition.startToCloseTimeout,
+            heartbeatTimeout: definition.heartbeatTimeout
+        };
+        runContext.getNextId = nextIdStub;
+        contextResolution.getContext = getContextStub;
+        mockObservableFactory.create = createStub;
+        mockSerializer.stringify = stringifyStub;
+        definition.serializer = mockSerializer;
+        const remoteActivityAdapter = new RemoteActivityAdapter(contextResolution, definition, testTaskList, mockObservableFactory);
+        remoteActivityAdapter.createObservable(testCallAttributes);
+        expect(createStub.getCall(0).args).to.be.eql([runContext, expectedScheduleParameters]);
+        expect(stringifyStub.getCall(0).args).to.be.eql([testCallAttributes]);
     });
 });
--- src/application/activity/adapters/remote-activity-adapter.ts
+++ src/application/activity/adapters/remote-activity-adapter.ts
@@ -4,21 +4,46 @@ import {ContextResolutionStrategy} from "../../context/resolution-strategies/res
 import {DecisionRunContext} from "../../context/decision-run-context";
 import {ScheduleActivityTaskDecisionAttributes} from "../../../aws/aws.types";
 import {ActivityDefinition} from "../activity-definition";
-import {Implementation} from "../../../implementation";
+import {ObservableFactory} from "../../observable-factory";
+import {DefaultRemoteObservableFactory} from "../observable/remote-activity-observable";
+import {Serializer} from "../serializer";
 
-export class RemoteActivityAdapter<T> implements ActivityAdapter<any[],T> {
 
-
+export class RemoteActivityAdapter<T> implements ActivityAdapter<any[],string> {
     constructor(private contextResolutionStrategy: ContextResolutionStrategy<DecisionRunContext>,
-                private activityDefinition: ActivityDefinition) {
+                private activityDefinition: ActivityDefinition,
+                private taskList: string,
+                private observableFactory?: ObservableFactory<string>) {
+        this.observableFactory = observableFactory || new DefaultRemoteObservableFactory();
     }
 
-    private createScheduleAttributes(callParams: any[]): ScheduleActivityTaskDecisionAttributes {
-        return null;
+    public createObservable(callParams: any[]): Observable<string> {
+        const context: DecisionRunContext = this.contextResolutionStrategy.getContext();
+        const activityId = context.getNextId();
+        const attributes = RemoteActivityAdapter.createScheduleAttributes(this.activityDefinition, this.taskList, callParams, activityId);
+        return this.observableFactory.create(context, attributes);
     }
 
 
-    createObservable(callParams: any[], observableConstructor?: Implementation<Observable<String>>): Observable<T> {
-        return null;
+    private static createScheduleAttributes(activityDefinition: ActivityDefinition, taskList: string, callParams: any[], activityId: string): ScheduleActivityTaskDecisionAttributes {
+        const serializer: Serializer = activityDefinition.serializer;
+        const input = serializer.stringify(callParams);
+        const attributes: ScheduleActivityTaskDecisionAttributes = {
+            activityType: {
+                name: activityDefinition.name,
+                version: activityDefinition.version
+            },
+            activityId: activityId,
+            input: input,
+            scheduleToCloseTimeout: activityDefinition.scheduleToCloseTimeout,
+            taskList: {
+                name: taskList
+            },
+            scheduleToStartTimeout: activityDefinition.scheduleToStartTimeout,
+            startToCloseTimeout: activityDefinition.startToCloseTimeout,
+            heartbeatTimeout: activityDefinition.heartbeatTimeout
+        };
+        return attributes;
     }
+
 }
